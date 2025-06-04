e02e531945d6c303b9fcb089d4a25d6de78c88aa
Jackson <jchayward1@gmail.com>
2025-06-04 12:41:25 -0700

    file for tracking code to test and debug hardware functionality

---
+++ test_hardware.cpp
@@ -0,0 +1,42 @@
+// #include <Servo.h>
+
+const int ledPin = 13;         // Built-in LED pin
+int centerPos = 90;      // Center position for servo
+int escPin = 3;
+const int servoPin = 12;
+const int frontEchoPin = 11;
+const int frontTrigPin = 10;
+const float soundSpeed = 0.034;
+
+
+
+void setup() {
+  pinMode(frontTrigPin, OUTPUT);
+  pinMode(frontEchoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void sendUltrasonicPulse(int trigPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+}
+
+void loop() {
+  // Turn LED on
+  sendUltrasonicPulse(frontTrigPin);
+  long duration = pulseIn(frontEchoPin, HIGH, 30000); // review this - pulseIn timeout probably can be much less than 1s
+  float distance = duration * 0.034 / 2;
+
+  // trigger light if within 20cm
+  if (distance > 0 && distance < 20) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(100);
+
+}
